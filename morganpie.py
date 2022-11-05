4e575857ff4b795ed214f369f3a146d2ab1e36bf
PotatoX8x <103386151+PotatoX8x@users.noreply.github.com>
2022-11-05 18:09:06 +0200

    Update morganpie.py

--- morganpie.py
+++ morganpie.py
@@ -1,9 +1,9 @@
 import numpy as np
 
-k=1000
+k = 1000
 a = np.random.default_rng().uniform(-1.0, 1.0, (k, 2))
-n=0
+n = 0
 for x, y in a:
-    if (x*x+y*y<=1):
-        n+=1
+    if (x * x + y * y <= 1):
+        n += 1
 print('Number of dots inside of circle: ', n ,'Pi = ', n/k*4)
